59b9b65917d9b8ed8f571d3c785bc1aadf878dec
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2015-07-17 12:05:23 +0200

    initial commit

---
+++ parser.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once("simple_html_dom.php");
+
+$files = file_get_contents("input.files");
+$filesArray = explode("\n", $files);
+$outBuffer = '';
+
+foreach ($filesArray as $htmlFile) {
+
+    echo 'Processing ' . $htmlFile . '...' . PHP_EOL;
+    $html = file_get_html($htmlFile);
+    $body = $html->find('body', 0);
+
+    $bodyHtml = str_get_html($body);
+
+    foreach ($bodyHtml->childNodes()[0]->childNodes() as $childs) {
+
+        //echo $childs . PHP_EOL;
+        //create processable child
+        $childsHtml = str_get_html($childs);
+
+
+
+        $outBuffer .= elementHandler($childsHtml, $outBuffer);
+
+    }
+
+    echo $outBuffer;
+
+    $outputName = 'out' . str_replace('.html', '.txt', $htmlFile);
+
+    file_put_contents($outputName, $outBuffer);
+
+    //
+    //echo parse()$bodyHtml;
+    /**
+     * process body
+     */
+
+}
+
+echo PHP_EOL . '...done. Created ' . $outputName . PHP_EOL;
+
+/**
+ * @param simple_html_dom $childsHtml
+ * @param string $tmpBuffer
+ * @return string
+ */
+function elementHandler($childsHtml, $tmpBuffer) {
+
+    $lis = $childsHtml->find('li');
+    foreach ($lis as $li) {
+        /** @var simple_html_dom_node $li */
+        $tmpBuffer .= '*' . $li->innertext();
+        /** @var simple_html_dom_node $next */
+        $next = $li->nextSibling();
+        if (strpos($next,"</p>") !== false) {
+            $tmpBuffer .= $next->innertext() . "\n";
+        }
+    }
+    $tmpBuffer .= "\n";
+    return $tmpBuffer;
+}
