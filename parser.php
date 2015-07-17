1657963b239f8e74f789c0a4acbc827f410798cf
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2015-07-17 14:06:14 +0200

    improved and fixed bugs, ul is now handled correctly, b tags are now handled

--- parser.php
+++ parser.php
@@ -12,17 +12,14 @@ foreach ($filesArray as $htmlFile) {
     $html = file_get_html($htmlFile);
     $body = $html->find('body', 0);
 
-    $bodyHtml = str_get_html($body);
-
-    foreach ($bodyHtml->childNodes()[0]->childNodes() as $childs) {
-
-        //echo $childs . PHP_EOL;
-        //create processable child
-        $childsHtml = str_get_html($childs);
+    foreach ($body->children as $childs) {
 
 
+        $tmpBuffer = '';
+        $tmpBuffer = elementHandler($childs, $tmpBuffer);
 
-        $outBuffer .= elementHandler($childsHtml, $outBuffer);
+
+        $outBuffer .= $tmpBuffer;
 
     }
     $outBuffer = htmlspecialchars_decode($outBuffer);
@@ -31,42 +28,44 @@ foreach ($filesArray as $htmlFile) {
 
     file_put_contents($outputName, $outBuffer);
 
-    //
-    //echo parse()$bodyHtml;
-    /**
-     * process body
-     */
 
 }
 
 echo PHP_EOL . '...done. Created ' . $outputName . PHP_EOL;
 
 /**
- * @param simple_html_dom $childsHtml
+ * @param simple_html_dom_node $childsHtml
  * @param string $tmpBuffer
  * @return string
  */
-function elementHandler($childsHtml, $tmpBuffer) {
+function elementHandler($childsHtml, $tmpBuffer, $depth = 0) {
 
-    $lis = $childsHtml->find('li');
-    foreach ($lis as $li) {
-        /** @var simple_html_dom_node $li */
-        $c = -1;
-        $tmp = $li;
-        while (!is_null($tmp->parentNode()) && strpos($tmp->parentNode(),"<ul>")==0) {
-            $c++;
-            $tmp = $tmp->parentNode();
-        }
-        for ($i = 0; $i < $c; $i++) {
-            $tmpBuffer .= '*' . $li->innertext();
-        }
+    foreach ($childsHtml->children as $child) {
+        /** @var simple_html_dom_node $child */
 
 
-        /** @var simple_html_dom_node $next */
-        $next = $li->nextSibling();
-        if (strpos($next,"</p>") !== false) {
-            $tmpBuffer .= $next->innertext() . "\n";
+        if ($child->tag == 'ul') {
+            $tmpBuffer = elementHandler($child, $tmpBuffer, ($depth+1));
+            continue;
         }
+
+        if ($child->tag == 'li') {
+            $tmpBuffer.='*';
+            for ($i = 0; $i < $depth; $i++) {
+                $tmpBuffer.='*';
+            }
+        } else if ($child->tag == 'p') {
+            $inner = $child->innertext();
+            $inner = str_replace("\t", '', $inner);
+            $inner = str_replace("\n", ' ', $inner);
+            $tmpBuffer .= $inner . "\n";
+        } else if ($child->tag == 'b') {
+            $tmpBuffer .= '===';
+            $tmpBuffer .= $child->innertext();
+            $tmpBuffer .= "===\n";
+        }
+
+
     }
     $tmpBuffer .= "\n";
     return $tmpBuffer;
